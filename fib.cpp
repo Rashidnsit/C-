0a3e282105e6f8ac3fffa6951d488f10ea04048a
RASHID <rashidsaifi604@gmail.com>
2021-04-25 01:51:10 +0530

    more prog

---
+++ fib.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void fib(int n){
+    int t1 =0;
+    int t2 =1;
+    int nextnum ;
+
+    for (int i = 0; i <= n; i++)
+    {
+        cout<<t1<<endl;
+        nextnum = t2+t1;
+        t1 =t2;
+        t2 = nextnum;
+    }
+    
+    return;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    fib(n);
+    return 0;
+}
