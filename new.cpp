60983fcae2cddf0f364d1165844de3dca8c6d11f
RASHID <rashidsaifi604@gmail.com>
2021-04-27 01:09:56 +0530

    array

---
+++ new.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int w;
+    cin>>w;
+    if(w%2 ==0 ){
+        if (w==2)
+        {
+           cout<<"no";
+        }
+        else{
+            cout<<"yes";
+        }  
+    }
+    else{
+        cout<<"no";
+    }
+   
+    return 0;
+}
